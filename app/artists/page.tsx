1977eea12250171aa13c7bcf956ed8dabb0de3f8
George Kariuki <gconsummer.gk@gmail.com>
2025-07-21 22:46:46 +0200

    Find events based on artist name

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,21 +1,71 @@
+"use client";
+import { useState, useEffect } from "react";
+import SearchBar from "@/components/SearchBar/SearchBar";
+import { ConcertEvent } from "@/types/Event";
+import { fetchArtistEvents } from "@/lib/fetchArtist";
+import EventCard from "@/components/EventCard/EventCard";
+
 export default function ArtistsPage() {
-    return (
-      <main className="container">
-        <section>
-          <h1>Featured Artists</h1>
-          <p>From rising stars to seasoned legends—meet the musicians moving Europe.</p>
-        </section>
-  
-        <section>
-          <h2>Genres We Spotlight</h2>
-          <p>Jazz, Indie, Classical, Techno, Soul, Folk and more.</p>
-        </section>
-  
-        <section>
-          <h2>Coming Soon</h2>
-          <p>Artist profiles and playlists will be added here shortly!</p>
-        </section>
-      </main>
-    );
+  const placeholder = "Search Events by Artist ...";
+
+  // Current artist in the form
+  const [artistQuery, setArtistQuery] = useState("Echo Pulse");
+
+  // Current Events with the artist
+  const [events, setEvents] = useState<ConcertEvent[]>([]);
+
+  // Loading toggles
+  const [loading, setLoading] = useState(false);
+
+  // Load events for the artists
+  useEffect(() => {
+    loadEvents(artistQuery);
+  }, []);
+
+  // Source from the database
+  async function loadEvents(artist: string) {
+    try {
+      setLoading(true);
+      const result = await fetchArtistEvents(artist);
+      setEvents(result);
+    } catch (error) {
+      console.error("Artist fetch failed:", error);
+    } finally {
+      setLoading(false);
+    }
   }
-  
+
+  return (
+    <main className="container">
+      <section>
+        <h1>Featured Artists</h1>
+        <p>
+          From rising stars to seasoned legends—meet the musicians moving
+          Europe.
+        </p>
+
+        {/* Search Bar */}
+        <h2>Search for your artist</h2>
+        <SearchBar
+          placeholder={placeholder}
+          query={artistQuery}
+          onChange={setArtistQuery}
+          onSubmit={() => loadEvents(artistQuery)}
+        />
+
+        {/* Load the data */}
+        {loading ? (
+          <p>Loading the events</p>
+        ) : events.length === 0 ? (
+          <p>
+            Sorry, there are no events for the artist {artistQuery} currently!
+          </p>
+        ) : (
+          events.map((event) => (
+            <EventCard event={event} key={event.id}/>
+          ))
+        )}
+      </section>
+    </main>
+  );
+}
