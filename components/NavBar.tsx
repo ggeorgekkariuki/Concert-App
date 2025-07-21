18d4ccbf6c23e6062b131e66fc3eb6e69d9971ed
George Kariuki <gconsummer.gk@gmail.com>
2025-07-21 17:04:32 +0200

    Update navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,15 +1,21 @@
+'use client';
 import Link from "next/link";
+import styles from "./NavBar.module.css";
+import { usePathname } from "next/navigation";
+
 
 export default function NavBar() {
+    const pathName = usePathname()
 
     return (
-        <nav>
-            <Link href={"/"}>Home</Link> | {" "}
-            <Link href={"/events"}>Events</Link> | {" "}
-            <Link href={"/artists"}>Artists</Link> | {" "}
-            <Link href={"/favorites"}>Favorites</Link> | {" "}
-            <Link href={"/about"}>About</Link> | {" "}
+        <nav className={styles.navbar}>
+            <Link href={"/"} className={ pathName === "/" ? styles.activeLink : styles.link}>Home</Link> | {" "}
+            <Link href={"/events"} className={ pathName === "/events" ? styles.activeLink : styles.link}>Events</Link> | {" "}
+            <Link href={"/artists"} className={ pathName === "/artists" ? styles.activeLink : styles.link}>Artists</Link> | {" "}
+            <Link href={"/favorites"} className={ pathName === "/favorites" ? styles.activeLink : styles.link}>Favorites</Link> | {" "}
+            <Link href={"/about"} className={ pathName === "/about" ? styles.activeLink : styles.link}>About</Link>
         </nav>
+
     )
 
 }
