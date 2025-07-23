4f668311a3a3c639f9e086737d0abb142ec51fee
George Kariuki <gconsummer.gk@gmail.com>
2025-07-23 22:11:34 +0200

    Add the Artists Directory to the Navigation

--- components/Navigation/NavBar.tsx
+++ components/Navigation/NavBar.tsx
@@ -1,21 +1,56 @@
-'use client';
+"use client";
 import Link from "next/link";
 import styles from "./NavBar.module.css";
 import { usePathname } from "next/navigation";
 
-
 export default function NavBar() {
-    const pathName = usePathname()
+  const pathName = usePathname();
 
-    return (
-        <nav className={styles.navbar}>
-            <Link href={"/"} className={ pathName === "/" ? styles.activeLink : styles.link}>Home</Link> | {" "}
-            <Link href={"/events"} className={ pathName === "/events" ? styles.activeLink : styles.link}>Events</Link> | {" "}
-            <Link href={"/artists"} className={ pathName === "/artists" ? styles.activeLink : styles.link}>Artists</Link> | {" "}
-            <Link href={"/favorites"} className={ pathName === "/favorites" ? styles.activeLink : styles.link}>Favorites</Link> | {" "}
-            <Link href={"/about"} className={ pathName === "/about" ? styles.activeLink : styles.link}>About</Link>
-        </nav>
-
-    )
-
+  return (
+    <nav className={styles.navbar}>
+      <Link
+        href={"/"}
+        className={pathName === "/" ? styles.activeLink : styles.link}
+      >
+        Home
+      </Link>{" "}
+      |{" "}
+      <Link
+        href={"/events"}
+        className={pathName === "/events" ? styles.activeLink : styles.link}
+      >
+        Events
+      </Link>{" "}
+      |{" "}
+      <Link
+        href={"/artists"}
+        className={pathName === "/artists" ? styles.activeLink : styles.link}
+      >
+        Artists
+      </Link>{" "}
+      |{" "}
+      <Link
+        href={"/artists/directory"}
+        className={
+          pathName === "/artists/directory" ? styles.activeLink : styles.link
+        }
+      >
+        Artists Directory
+      </Link>{" "}
+      |{" "}
+      <Link
+        href={"/favorites"}
+        className={pathName === "/favorites" ? styles.activeLink : styles.link}
+      >
+        Favorites
+      </Link>{" "}
+      |{" "}
+      <Link
+        href={"/about"}
+        className={pathName === "/about" ? styles.activeLink : styles.link}
+      >
+        About
+      </Link>
+    </nav>
+  );
 }
