54ba55890402417e5c28ab07e55e14b624dfe154
George Kariuki <gconsummer.gk@gmail.com>
2025-07-24 00:54:58 +0200

    Add sorting to the directory page

--- app/artists/directory/page.tsx
+++ app/artists/directory/page.tsx
@@ -5,6 +5,7 @@ import { fetchArtistsByGenre } from "@/lib/fetchArtistsByGenre";
 import styles from "./Directory.module.css";
 import GenreFilter from "@/components/Genres/GenreFilter";
 import ArtistCard from "@/components/Artists/ArtistCard";
+import SortMenu from "@/components/SortMenu/SortMenu";
 
 export default function ArtistDirectoryPage() {
   // Maintain the genre state
@@ -13,17 +14,44 @@ export default function ArtistDirectoryPage() {
   const [artists, setArtists] = useState<Artist[]>([]);
   // Loading state
   const [loading, setLoading] = useState(false);
+  // Keep track of the sort key as either name or country
+  const [sortKey, setSortKey] = useState <"name"|"country">("name");
+
+  // A typed accessor to efficiently sort the returned artist array because
+  // Typescript rejected [...results].sort((a, b) => a[sortKey].localCompare ...)
+  const getSortableValue = (artist: Artist, key: "name" | "country") => {
+    return artist[key] || "";
+  };
+
+  console.log(artists);
 
   // Rerun if  the genre component changes
+  // Rerun if the sort key changes
   useEffect(() => {
     loadArtists();
-  }, [genre]);
+  }, [genre, sortKey]);
 
   async function loadArtists() {
     setLoading(true);
-    const query = await fetchArtistsByGenre(genre);
-    setArtists(query);
+    const results = await fetchArtistsByGenre(genre);
+    const sortedResults = [...results].sort((a, b) =>
+      getSortableValue(a, sortKey ).localeCompare(
+        getSortableValue(b, sortKey)
+      )
+    );
+    setArtists(sortedResults);
     setLoading(false);
+  }
+
+  /**
+   The error 
+   "Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'Artist'.
+  No index signature with a parameter of type 'string' was found on type 'Artist'." when I ran the line
+  const sortedResults = [...results].sort((a, b) => a[sortKey].localCompare(b[sortKey]))
+   */
+
+  function handleSortKeyChange(sortKey: string) {
+    setSortKey(sortKey as "name" | "country");
   }
 
   return (
@@ -35,6 +63,8 @@ export default function ArtistDirectoryPage() {
           picks.
         </p>
         <GenreFilter genre={genre} onChange={setGenre} />
+        {/* Sorting here */}
+        <SortMenu sortKey={sortKey} onChange={handleSortKeyChange} />
       </section>
 
       <section className={styles.gridSection}>
